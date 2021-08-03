6b71577d2f4d14a196848be8edeea4c4a30e53b8
Victor Kamei Kay <kameikay@gmail.com>
2021-08-02 21:00:15 -0300

    deleted empty files, deleted scripts with no functions (header), delete not used images

--- src/public/scripts/header.js
+++ src/public/scripts/header.js
@@ -3,21 +3,7 @@ const menuItems = document.querySelector(".menu-items");
 const menuLinks = document.querySelectorAll(".menu-items li");
 const header = document.querySelector('header')
 
-const sections = {
-    hero: ".hero",
-    quemSomos: ".quem-somos",
-    beneficios: ".beneficios",
-    comoFuncionam: ".como-funcionam",
-    simule: ".simule",
-    portfolio: ".portfolio",
-    contato: ".contato",
-};
 
-function smoothFunction(elementTargetClass) {
-    document.querySelector(`${elementTargetClass}`).scrollIntoView({
-        behavior: "smooth",
-    });
-}
 // Open hamburger menu
 function handlerMenu() {
     menuItems.classList.toggle("active");
@@ -28,34 +14,7 @@ menuMobile.addEventListener("click", handlerMenu);
 
 menuLinks.forEach((link) => {
     link.addEventListener("click", () => {
-        if (link.innerHTML === menuLinks[0].innerHTML) {
-            smoothFunction(sections.hero);
             handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[1].innerHTML) {
-            smoothFunction(sections.quemSomos);
-            handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[2].innerHTML) {
-            smoothFunction(sections.beneficios);
-            handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[3].innerHTML) {
-            smoothFunction(sections.comoFuncionam);
-            handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[4].innerHTML) {
-            smoothFunction(sections.simule);
-            handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[5].innerHTML) {
-            smoothFunction(sections.portfolio);
-            handlerMenu();
-        }
-        if (link.innerHTML === menuLinks[6].innerHTML) {
-            smoothFunction(sections.contato);
-            handlerMenu();
-        }
     });
 });
 
