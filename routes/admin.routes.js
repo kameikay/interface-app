f5721dd2273e680a5e4b8f07c065f1dd243424c4
Victor Kamei Kay <kameikay@gmail.com>
2021-08-08 18:30:42 -0300

    removed empty folders

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const adminRoutes = express.Router();
+const upload = require('multer')
 
-const api = require("../src/controllers/api");
 const adminController = require("../src/controllers/adminController");
 const loginController = require("../src/controllers/loginController");
 
 const errorController = require("../src/controllers/errorController");
 
 const { loginRequired } = require("../src/middlewares/middleware");
+
+// Multer functions
+
 
 adminRoutes.get("/", loginController.loginForm);
 adminRoutes.post("/", loginController.login);
@@ -20,11 +23,6 @@ adminRoutes.get("/admin/edit/:id", loginRequired, adminController.editPage);
 adminRoutes.post("/admin/edit/:id", loginRequired, adminController.editPost);
 adminRoutes.get("/admin/delete/:id", loginRequired, adminController.deletePost);
 
-// adminRoutes.get("/api", api.index);
-// adminRoutes.get("/api/:id", api.show);
-// adminRoutes.post("/api", api.store);
-// adminRoutes.put("/api/:id", api.update);
-// adminRoutes.delete("/api/:id", api.delete);
 
 adminRoutes.get("/*", errorController.errorPage);
 
